06f950918e62c102c9b5e9de04aa86c6963153c8
Andres Avila <andrs3662@gmail.com>
2022-06-17 11:21:46 -0500

    some notes

--- src/main/java/com/mycompany/sistemagestionfinal/SistemagestionFinal.java
+++ src/main/java/com/mycompany/sistemagestionfinal/SistemagestionFinal.java
@@ -6,6 +6,9 @@
 package com.mycompany.sistemagestionfinal;
 
 import com.mycompany.sistemagestionfinal.forms.Formulario;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  *
@@ -17,4 +20,26 @@ public class SistemagestionFinal {
         Formulario ventana=new Formulario();
         ventana.show();
     }
+    
+//    public static void main(String[] args) throws IOException {
+//
+//        System.out.println("Ingresar un valor:");
+//        Integer valor = cargarNumero();
+//        Boolean esPar = valor % 2 == 0;
+//        if (esPar) {
+//            System.out.println("Es par el numero: " + valor);
+//        } else {
+//            System.out.println("No es par el numero: " + valor);
+//        }
+//
+//    }
+//
+//    public static Integer cargarNumero() throws IOException {
+//        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+//        BufferedReader reader = new BufferedReader(inputStreamReader);
+//        String strNumero = reader.readLine();
+//        Integer numero = Integer.parseInt(strNumero);
+//
+//        return numero;
+//    }
 }
